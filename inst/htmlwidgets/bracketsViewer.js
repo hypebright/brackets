dfa0aa8ec65438e98a4e1601b26694eb7e434a1f
Veerle van Leemput <info@hypebright.nl>
2024-02-19 14:59:21 +0100

    add element id

--- inst/htmlwidgets/bracketsViewer.js
+++ inst/htmlwidgets/bracketsViewer.js
@@ -1,34 +1,35 @@
 HTMLWidgets.widget({
+  name: "bracketsViewer",
 
-  name: 'bracketsViewer',
-
-  type: 'output',
+  type: "output",
 
   factory: function(el, width, height) {
-
     // TODO: define shared variables for this instance
 
     return {
-
       renderValue: function(opts) {
         
-        el.innerHTML = '<div id="bracketsViewerExample" class="brackets-viewer"></div>'
+        const elementId = el.id + "-brackets-viewer";
+        
+        el.innerHTML = '<div id="' + elementId + '" class="brackets-viewer"></div>';
 
-        window.bracketsViewer.render({
-          stages: opts.data.stage,
-          matches: opts.data.match,
-          matchGames: opts.data.match_game,
-          participants: opts.data.participant
-        });
-
+        window.bracketsViewer.render(
+          {
+            stages: opts.data.stage,
+            matches: opts.data.match,
+            matchGames: opts.data.match_game,
+            participants: opts.data.participant
+          },
+          {
+            selector: "#" + elementId
+          }
+        );
       },
 
       resize: function(width, height) {
-
         // TODO: code to re-render the widget with a new size
-
       }
-
     };
   }
 });
+
